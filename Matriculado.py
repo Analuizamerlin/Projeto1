248712cf8c71ed019f04378a07a8fce1ad11d9d1
Ana Luiza <analuizamerlin01@gmail.com>
2023-08-24 10:54:41 -0300

    Incrementei o texto de resposta

--- Matriculado.py
+++ Matriculado.py
@@ -1,13 +1,16 @@
 with open('./nomes.txt','r') as reader:
    arquivo = reader.readlines()
+
 k = 0
+
 nome = input("Nome completo: ")
 nome = nome.lower()
 
 for line in arquivo:
    line = line.lower()
    if nome in line:
-      print("Você está matriculado!")
+      print("Você está matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!")
       k=k+1
+
 if k == 0:
-   print("Você não está matriculado!")
+   print("Você não está matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!")
