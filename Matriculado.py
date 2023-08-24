127f7f5d878e0ba42cfd524a7f8541eb637e8b4e
Ana Luiza <analuizamerlin01@gmail.com>
2023-08-24 18:48:00 -0300

    Printa a resposta com o nome da pessoa.

--- Matriculado.py
+++ Matriculado.py
@@ -4,15 +4,15 @@ with open('./nomes.txt','r') as reader:
 k = 0
 i = 0
 
-nome = input("Nome completo: ")
+nome = input("Insira oome completo: ")
 nome = nome.lower()
 
 for line in arquivo:
    line = line.lower()
    i= i +1
    if nome in line:
-      print("Você foi o",i,"° matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!")
+      print("\n", line, "foi o",i,"° matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!\n")
       k=k+1
 
 if k == 0:
-   print("Você não está matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!")
+   print("\n",nome,"não está matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!\n")
