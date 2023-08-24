fc5606ef3f45eb019dc84d6c5911115101a187b5
Ana Luiza <analuizamerlin01@gmail.com>
2023-08-24 11:14:45 -0300

    Acrescentei a posição que o aluno foi matriculado

--- Matriculado.py
+++ Matriculado.py
@@ -2,14 +2,16 @@ with open('./nomes.txt','r') as reader:
    arquivo = reader.readlines()
 
 k = 0
+i = 0
 
 nome = input("Nome completo: ")
 nome = nome.lower()
 
 for line in arquivo:
    line = line.lower()
+   i= i +1
    if nome in line:
-      print("Você está matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!")
+      print("Você foi o",i,"° matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!")
       k=k+1
 
 if k == 0:
