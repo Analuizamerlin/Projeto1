b41c10bbe17c556e4905219eb1ab16979b843444
Ana Luiza <analuizamerlin01@gmail.com>
2023-08-25 13:08:11 -0300

    Cadastramento de novos alunos funcionando

--- Matriculado.py
+++ Matriculado.py
@@ -1,5 +1,6 @@
-with open('./nomes.txt','r') as reader:
-   arquivo = reader.readlines()
+file = open('nomes.txt', 'r')
+lines = file.readlines()
+file.close()
 
 k = 0
 i = 0
@@ -11,25 +12,19 @@ if n == 1:
    nome = input("\nInsira nome completo: ")
    nome = nome.lower()
 
-   for line in arquivo:
-      line = line.lower()
+   for min in lines:
+      min = min.lower()
       i= i +1
-      if nome in line:
-         print("\n", line, "foi o",i,"° matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!\n")
+      if nome in min:
+         print("\n", min, "foi o",i,"° matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!\n")
          k=k+1
    if k == 0:
       print("\n",nome,"não está matriculado na turma 1 do curso de Pontapé Inicial Desenvolvedor Python!\n")
 
 if n == 2:
-   with open('./nomes.txt','a') as arquivo:
-      arquivo.write = input("Insira nome completo do novo aluno: ")
-      arquivo.close()
-      with open('./nomes.txt','r') as reader:
-         arquivo = reader.readlines()
+   novo = input("Insira o nome completo do novo aluno: ")
+   lines.insert(len(lines)+1,"\n" + novo)
 
-      for line in arquivo:
-         print(line)
-      
-      
-
-      
+   file = open('nomes.txt', 'w')
+   file.writelines(lines)
+   file.close()
